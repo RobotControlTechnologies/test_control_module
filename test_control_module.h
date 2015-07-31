8a87c6f6ea3560f98a9bc6e5c7a3268d2207cda2
Mikhail Tulkin <tulkin@robotct.ru>
2015-07-31 11:20:19 +0500

    Formatted to google style code.

--- test_control_module.h
+++ test_control_module.h
@@ -1,39 +1,39 @@
 #ifndef TEST_CONTROL_MODULE_H
-#define	TEST_CONTROL_MODULE_H
-
+#define TEST_CONTROL_MODULE_H
 
 class TestControlModule : public ControlModule {
-	AxisData **test_axis;
-	colorPrintfModuleVA_t *colorPrintf_p;
+  AxisData **test_axis;
+  colorPrintfModuleVA_t *colorPrintf_p;
 
-public:
-	TestControlModule(){};
+ public:
+  TestControlModule(){};
 
-	//init
-	const char *getUID();
-	void prepare(colorPrintfModule_t *colorPrintf_p, colorPrintfModuleVA_t *colorPrintfVA_p);
+  // init
+  const char *getUID();
+  void prepare(colorPrintfModule_t *colorPrintf_p,
+               colorPrintfModuleVA_t *colorPrintfVA_p);
 
-	//compiler only
-	AxisData** getAxis(unsigned int *count_axis);
-	void *writePC(unsigned int *buffer_length);
+  // compiler only
+  AxisData **getAxis(unsigned int *count_axis);
+  void *writePC(unsigned int *buffer_length);
 
-	//intepreter - devices
-	int init();
-	void execute(sendAxisState_t sendAxisState);
-	void final() {};
+  // intepreter - devices
+  int init();
+  void execute(sendAxisState_t sendAxisState);
+  void final(){};
 
-	//intepreter - program & lib
-	void readPC(void *buffer, unsigned int buffer_length);
+  // intepreter - program & lib
+  void readPC(void *buffer, unsigned int buffer_length);
 
-	//intepreter - program
-	int startProgram(int uniq_index);
-	int endProgram(int uniq_index);
+  // intepreter - program
+  int startProgram(int uniq_index);
+  int endProgram(int uniq_index);
 
-	//destructor
-	void destroy();
-	~TestControlModule() {}
+  // destructor
+  void destroy();
+  ~TestControlModule() {}
 
-	void colorPrintf(ConsoleColor colors, const char *mask, ...);
+  void colorPrintf(ConsoleColor colors, const char *mask, ...);
 };
 
-#endif	/* TEST_CONTROL_MODULE_H */
+#endif /* TEST_CONTROL_MODULE_H */
